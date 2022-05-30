f3d56e2a9c13f65d889bac291774287a3bde047f
Woody <60283977+YH3041@users.noreply.github.com>
2022-05-30 14:30:46 +0900

    토스트 메시지 구현 및 적용 (#8)
    * Style: 토스트 메시지 구현 및 적용

--- core/api/login/loginApi.ts
+++ core/api/login/loginApi.ts
@@ -1,12 +1,9 @@
-import { Cookies } from "react-cookie";
 import Methods from "@/util/methods";
 import comonApi from "@/core/api/api";
 import { IUser } from "@/interface/user";
 
 // 로그인 API 구현
 export default async function loginApi(user: IUser) {
-  const cookies = new Cookies();
-
   const response = await comonApi(Methods.POST, process.env.LOGIN_API_URL, {
     headers: {
       "Content-Type": "application/json",
@@ -14,12 +11,5 @@ export default async function loginApi(user: IUser) {
     body: user,
   });
 
-  if (response.status !== 200) throw new Error(response.statusText);
-
-  // TODO 추후에 header.setCookie로 수정 예정
-  cookies.set(user.email, user.name, {
-    path: "/",
-    secure: true,
-    httpOnly: true,
-  });
+  return response;
 }
