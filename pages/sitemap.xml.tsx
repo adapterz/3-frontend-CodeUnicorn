40dc87f0c5e3a7311d7d15bb7b78d27b2a31fa9d
YH3041 <mmj2rv9@gmail.com>
2022-07-02 20:55:51 +0900

    sitemap 이슈 해결

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,37 +1,27 @@
 import { categories } from "@/components/Catagories";
 import axios from "axios";
-import fs from "fs";
-import { GetServerSideProps, GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import { getServerSideSitemap } from "next-sitemap";
 
 const DOMAIN = "codeunicorn.kr";
 
-// export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const {
     data: { courseCount },
   } = await axios.get("https://api.codeunicorn.kr/courses/all");
 
+  const defaultSitemap = [
+    "codeunicorn.kr",
+    "codeunicorn.kr/courses",
+    "codeunicorn.kr/join",
+    "codeunicorn.kr/login",
+  ];
+
   const staticPages = [];
 
-  // const staticPages = fs
-  //   .readdirSync("pages")
-  //   .filter((staticPage) => {
-  //     return ![
-  //       "_app.tsx",
-  //       "_document.js",
-  //       "sitemap.xml.tsx",
-  //       "404.tsx",
-  //       "privacy.tsx",
-  //       "terms-of-service.tsx",
-  //       "courses.tsx",
-  //       "api",
-  //       "users",
-  //     ].includes(staticPage);
-  //   })
-  //   .map((staticPagePath) => {
-  //     return `${DOMAIN}/${staticPagePath}`;
-  //   });
+  defaultSitemap.map((sitemap) => {
+    staticPages.push(sitemap);
+  });
 
   categories.map((category) =>
     staticPages.push(
@@ -46,30 +36,11 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const fields = staticPages.map((page) => ({
     loc: `${page.replace(".tsx", "")}`,
     lastmod: new Date().toISOString(),
+    changefreq: "weekly",
+    priority: "1.0",
   }));
 
-  //   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  // <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  //   ${staticPages
-  //     .map((url) => {
-  //       return `
-  //         <url>
-  //           <loc>${url.replace(".tsx", "")}</loc>
-  //           <lastmod>${new Date().toISOString()}</lastmod>
-  //           <changefreq>monthly</changefreq>
-  //           <priority>1.0</priority>
-  //         </url>
-  //       `;
-  //     })
-  //     .join("")}
-  // </urlset>
-  // `;
-
-  // res.write(sitemap);
-  // res.end();
-
-  return getServerSideSitemap(ctx, fields);
+  return getServerSideSitemap(ctx, fields as any);
 };
 
 export default () => {};
-// export default SiteMap;
