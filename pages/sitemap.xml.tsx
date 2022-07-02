ee83d7db249fd36d9c10c5e7720069be6365054d
YH3041 <mmj2rv9@gmail.com>
2022-07-02 20:23:40 +0900

    sitemap 배포환경 테스트

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,5 +1,6 @@
 import { categories } from "@/components/Catagories";
 import axios from "axios";
+import fs from "fs";
 import { GetServerSideProps } from "next";
 
 const DOMAIN = "codeunicorn.kr";
@@ -7,64 +8,66 @@ const DOMAIN = "codeunicorn.kr";
 const SiteMap = () => {};
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const fs = require("fs");
+  try {
+    const {
+      data: { courseCount },
+    } = await axios.get("https://api.codeunicorn.kr/courses/all");
 
-  // const {
-  //   data: { courseCount },
-  // } = await axios.get("https://api.codeunicorn.kr/courses/all");
-
-  const staticPages = fs
-    .readdirSync("pages")
-    .filter((staticPage) => {
-      return ![
-        "_app.tsx",
-        "_document.js",
-        "sitemap.xml.tsx",
-        "404.tsx",
-        "privacy.tsx",
-        "terms-of-service.tsx",
-        "courses.tsx",
-        "api",
-        "users",
-      ].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-      return `${DOMAIN}/${staticPagePath}`;
-    });
-
-  categories.map((category) =>
-    staticPages.push(
-      `${DOMAIN}/courses?category=${category.key}&amp;sortby=popular&amp;page=1`,
-    ),
-  );
-
-  // for (let i = 1; i < courseCount; i++) {
-  //   staticPages.push(`${DOMAIN}/courses/${i}`);
-  // }
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${staticPages
-      .map((url) => {
-        return `
-          <url>
-            <loc>${url.replace(".tsx", "")}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>monthly</changefreq>
-            <priority>1.0</priority>
-          </url>
-        `;
+    const staticPages = fs
+      .readdirSync("pages")
+      .filter((staticPage) => {
+        return ![
+          "_app.tsx",
+          "_document.js",
+          "sitemap.xml.tsx",
+          "404.tsx",
+          "privacy.tsx",
+          "terms-of-service.tsx",
+          "courses.tsx",
+          "api",
+          "users",
+        ].includes(staticPage);
       })
-      .join("")}
-  </urlset>
-  `;
+      .map((staticPagePath) => {
+        return `${DOMAIN}/${staticPagePath}`;
+      });
 
-  res.write(sitemap);
-  res.end();
+    categories.map((category) =>
+      staticPages.push(
+        `${DOMAIN}/courses?category=${category.key}&amp;sortby=popular&amp;page=1`,
+      ),
+    );
 
-  return {
-    props: {},
-  };
+    for (let i = 1; i < courseCount; i++) {
+      staticPages.push(`${DOMAIN}/courses/${i}`);
+    }
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${staticPages
+    .map((url) => {
+      return `
+        <url>
+          <loc>${url.replace(".tsx", "")}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>1.0</priority>
+        </url>
+      `;
+    })
+    .join("")}
+</urlset>
+`;
+
+    res.write(sitemap);
+    res.end();
+
+    return {
+      props: {},
+    };
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export default SiteMap;
